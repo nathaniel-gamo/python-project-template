ab9f0b381978607f5b339913a8a3632530458a5b
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2025-04-21 02:21:35 +0800

    added alias of scr.config, updated variable annotation of LOG_FILE_NAME

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import logging
 
-import src.config
+import src.config as config
 
 def main() -> None:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        filename=f"{src.config.LOG_FILE_NAME}"
+        filename=f"{config.LOG_FILE_NAME}"
     )
-
-    logging.info("test")
 
 if __name__ == "__main__":
     main()
--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LOG_FILE_NAME: str = os.environ.get("LOG_FILE_NAME")
+LOG_FILE_NAME: str | None = os.environ.get("LOG_FILE_NAME")
