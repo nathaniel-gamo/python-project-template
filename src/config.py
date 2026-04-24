cb8c6c009e58447fb82025264121405561e47eab
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2026-04-25 03:25:10 +0800

    removed duplicate expression

--- src/config.py
+++ src/config.py
@@ -15,14 +15,13 @@ def _get_dotenv_path() -> str:
 def load_config(dotenv_path: str = "", 
                 dotenv_only: bool = True) -> dict[str, str | None]:
     
+    dotenv_path = (dotenv_path 
+                   if os.path.isfile(dotenv_path) 
+                   else _get_dotenv_path())
+    
     if dotenv_only:
-        return dotenv_values(dotenv_path=dotenv_path 
-                             if os.path.isfile(dotenv_path) 
-                             else _get_dotenv_path())
+        return dotenv_values(dotenv_path)
 
-    load_dotenv(dotenv_path=dotenv_path 
-                if os.path.isfile(dotenv_path) 
-                else _get_dotenv_path(), 
-                override=True)
+    load_dotenv(dotenv_path, override=True)
 
     return {key: value for key, value in os.environ.items()}
